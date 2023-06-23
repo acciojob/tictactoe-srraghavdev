923e16ccf2b7ade2ef47635383814d653c1ae23e
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-24 02:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function dynamic(){
+	const parent = document.getElementById("container")
+	const player1ref = document.getElementById("player-1")
+	const player2ref = document.getElementById("player-2")
+	const player1= player1ref.value
+	const player2= player2ref.value
+	const landingpageref = document.getElementById("landingpage")
+	landingpageref.remove()
+	const gridconatiner = document.createElement("div")
+	
+}
