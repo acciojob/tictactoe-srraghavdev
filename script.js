87c59aca68904e6cb8658f6e58108f62d76e1f70
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-03 23:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,141 +1,99 @@
-let count=1
-let prr=[]
-function dynamic(){
-	const parent = document.getElementsByClassName("container")[0]
-	const player1ref = document.getElementById("player1")
-	const player2ref = document.getElementById("player2")
-	const player1= player1ref.value
-	const player2= player2ref.value
-	const landingpageref = document.getElementById("landingpage")
-	prr.push(player1)
-	prr.push(player2)
-	landingpageref.remove()
-	let gridconatiner = document.createElement("div")
-	parent.appendChild(gridconatiner)
-	let message=document.createElement("div")
-	message.className="message"
-	message.innerText=`${player1}, you're up`
-	let count=0;
-	gridconatiner.append(message)
-	let grider= document.createElement("div")
-	grider.className="grider"
-	gridconatiner.append(grider)
-	let gridarray=[]
-	for(let i=0;i<9;i++){
-		let create=document.createElement("div")
-		create.className="gridcell"
-		create.id=`${i+1}`
-		grider.append(create)
-		gridarray.push(create)
-		create.addEventListener("mousedown",populate)
-	}	
-}	
+const submit = document.getElementById("submit");
+const game = document.querySelector(".game");
+const details = document.querySelector(".details");
+const message = document.querySelector(".message");
 
-function populate(obj){
-	if(obj.target.innerText!=""){
-		return
-	}
-	const mess= document.getElementsByClassName("message")[0]
-	if(mess.innerText===`${prr[1]} congratulations you won!` || mess.innerText===`${prr[0]} congratulations you won!`){
-		return
-	}
-	if(count%2===0){
-		mess.innerText=`${prr[0]}, you're up`
-		obj.target.innerText="o"
-	}
-	else{
-		mess.innerText=`${prr[1]}, you're up`
-		obj.target.innerText="x"
-	}
-	count++
-	const element = document.getElementById(obj.target.id)
-	checkforwin()
+let player1 = "";
+let player2 = "";
+let playGame = true;
+let activePlayer = 0;
+// 0 for player 1 and 1 for player 2
+
+function showMessage(msg) {
+  message.innerHTML = `<h3> ${msg}</h3>`;
 }
-function checkforwin(){
-	const el1= document.getElementById("1")
-	const el2= document.getElementById("2")
-	const el3= document.getElementById("3")
-	const el4= document.getElementById("4")
-	const el5= document.getElementById("5")
-	const el6= document.getElementById("6")
-	const el7= document.getElementById("7")
-	const el8= document.getElementById("8")
-	const el9= document.getElementById("9")
-	let arr=[]
-	arr.push([el1.innerText,el2.innerText,el3.innerText])
-	arr.push([el6.innerText,el5.innerText,el4.innerText])
-	arr.push([el7.innerText,el8.innerText,el9.innerText])
-	const mess= document.getElementsByClassName("message")[0]
-	if((arr[0][0]==="x" &&arr[0][1]==="x" &&arr[0][2]==="x") || (arr[0][0]==="o" &&arr[0][1]==="o" &&arr[0][2]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[1][0]==="x" &&arr[1][1]==="x" &&arr[1][2]==="x") || (arr[1][0]==="o" &&arr[1][1]==="o" &&arr[1][2]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won1`
-		}
-		return
-	}
-	if((arr[2][0]==="x" &&arr[2][1]==="x" &&arr[2][2]==="x") || (arr[2][0]==="o" &&arr[2][1]==="o" &&arr[2][2]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[0][0]==="x" &&arr[1][0]==="x" &&arr[2][0]==="x") || (arr[0][0]==="o" &&arr[1][0]==="o" &&arr[2][0]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[0][1]==="x" &&arr[1][1]==="x" &&arr[2][1]==="x") || (arr[0][1]==="o" &&arr[1][1]==="o" &&arr[2][1]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[0][2]==="x" &&arr[1][2]==="x" &&arr[2][2]==="x") || (arr[0][2]==="o" &&arr[1][2]==="o" &&arr[2][2]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[0][0]==="x" &&arr[1][1]==="x" &&arr[2][2]==="x") || (arr[0][0]==="o" &&arr[1][1]==="o" &&arr[2][2]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
-	if((arr[0][2]==="x" &&arr[1][2]==="x" &&arr[2][0]==="x") || (arr[0][2]==="o" &&arr[1][1]==="o" &&arr[2][0]==="o")){
-		if(count%2===0){
-			mess.innerText=`${prr[0]} congratulations you won!`
-		}
-		else{
-			mess.innerText=`${prr[1]} congratulations you won!`
-		}
-		return
-	}
+
+function updateActivePlayer(){
+    if(activePlayer===0)
+        activePlayer=1
+    else
+        activePlayer=0
 }
+
+function initializeGame() {
+  player1 = document.getElementById("player1").value;
+  player2 = document.getElementById("player2").value;
+
+  activePlayer = 0;
+
+  details.style.display = "none";
+  game.style.display = "flex";
+
+  // Append game-slots to game div
+  showMessage(`${activePlayer === 0 ? player1 : player2}, you're up`);
+
+  for (let i = 1; i <= 9; i++) {
+    const div = document.createElement("div");
+
+    div.id = i;
+    div.classList.add("game-slot");
+
+    // Add event listener for clicking
+    div.addEventListener("click", function () {
+
+      if(!div.innerText && playGame){
+        div.innerText = activePlayer === 0 ? "x" : "o";
+        const winner = checkWinner()
+
+        if(!winner){
+          updateActivePlayer();
+          showMessage(`${activePlayer === 0 ? player1 : player2}, you're up`)
+        }else{
+          showMessage(`${activePlayer===0 ? player1: player2} congratulations you won!`)
+        }
+      }
+
+
+    });
+
+    game.appendChild(div);
+  }
+}
+
+const winningSequences = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+function checkWinner(){
+    let winnerBool = false
+
+    for(let i=0;i<winningSequences.length;i++){
+      const winningCombo = winningSequences[i];
+
+      const cell1 = document.getElementById(winningCombo[0]+1)
+      const cell2 = document.getElementById(winningCombo[1]+1)
+      const cell3 = document.getElementById(winningCombo[2]+1)
+
+      const val1 = cell1.innerText
+      const val2 = cell2.innerText
+      const val3 = cell3.innerText
+
+      if(val1===val2 && val2===val3 && val1!=''){
+        winnerBool = true
+        playGame = false
+        cell1.style.backgroundColor="purple"
+        cell2.style.backgroundColor="purple"
+        cell3.style.backgroundColor="purple"
+        break;
+      }
+    }
+    return winnerBool
+}
+
+submit.addEventListener("click", initializeGame);
